0712963431d47cea14f6f29c3f47ea29b53af8e9
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2023-10-04 14:22:08 +0900

    initai commit

---
+++ iOSSampleRepository/iOSSampleRepository/iOSSampleRepositoryApp.swift
@@ -0,0 +1,18 @@
+//
+//  iOSSampleRepositoryApp.swift
+//  iOSSampleRepository
+//
+//  Created by 長谷川拓海 on 2023/10/04.
+//  Copyright (c) 2023 *ReNKCHANNEL*. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iOSSampleRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
